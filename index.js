3cdf344bdd14b88e4d35887c13c83ffbc389827a
Samuel Thng <samuel@thng.sg>
2021-10-09 01:08:40 +0800

    Added: Catchall for homeassistant apis.

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ import { HA_AUTH_HEADER, HA_URL, PORT } from './constants';
 
 const app = express();
 
-app.get('/api/config', async (req, res) => {
+app.get('/homeassistant*', async (req, res) => {
+	const { originalUrl } = req;
+	const query = originalUrl.split('/homeassistant')[1];
+	if (!query) res.status(400).send({ message: "Invalid endpoint." });
 	try {
-		const response = await axios.get(`${HA_URL}/config`, HA_AUTH_HEADER);
+		const response = await axios.get(`${HA_URL}${query}`, HA_AUTH_HEADER);
 		res.status(response.status).send(response.data);
 	} catch (error) {
 		res.status(400).send(error);
