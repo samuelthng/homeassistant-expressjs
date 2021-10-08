a9e04fc489f2f3cf6e53eed7f5b66c2296e0022d
Samuel Thng <samuel@thng.sg>
2021-10-09 00:47:18 +0800

    Updated: Use axios.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 
 const HA_URL = 'http://supervisor/core/api';
 const PORT = 3000;
@@ -6,9 +7,8 @@ const PORT = 3000;
 const app = express();
 
 app.get('/test', async (req, res) => {
-	const response = await fetch(HA_URL, { method: 'GET' });
-	const data = await response.json();
-	res.status(response.status).send(data);
+	const response = await axios.get(HA_URL);
+	res.status(response.status).send(response.data);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}…`));
