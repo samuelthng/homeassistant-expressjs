1173796a1bbe3d6dfb5a754f143ec7149e886645
Samuel Thng <samuel@thng.sg>
2021-10-09 01:33:08 +0800

    Updated: WS url

--- constants.js
+++ constants.js
@@ -4,7 +4,7 @@ export const HA_BASE = `http://supervisor/core`;
 
 export const HA_URL = `${HA_BASE}/api`;
 
-export const HA_WS = `${HA_BASE}/websocket`;
+export const HA_WS = `ws://supervisor/core/websocket`;
 
 export const HA_AUTH_MESSAGE = { "type": "auth", "access_token": SUPERVISOR_TOKEN }
 
